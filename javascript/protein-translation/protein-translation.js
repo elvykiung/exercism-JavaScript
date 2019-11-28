26f1a431d8459d077b60d153e3a8f44f4d52e16d
Elvy_kiung <kiungh22@gmail.com>
2019-11-27 16:04:49 -0800

    exercise protien-transalation

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -3,6 +3,45 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+export const translate = rna => {
+  const translate = [];
+  const protein = {
+    AUG: "Methionine",
+    UUU: "Phenylalanine",
+    UUC: "Phenylalanine",
+    UUA: "Leucine",
+    UUG: "Leucine",
+    UCU: "Serine",
+    UCC: "Serine",
+    UCA: "Serine",
+    UCG: "Serine",
+    UAU: "Tyrosine",
+    UAC: "Tyrosine",
+    UGU: "Cysteine",
+    UGC: "Cysteine",
+    UGG: "Tryptophan",
+    UAA: "STOP",
+    UAG: "STOP",
+    UGA: "STOP"
+  };
+
+  if (!rna) {
+    return [];
+  }
+
+  for (let i = 0; i < rna.length; i += 3) {
+    const codon = rna.slice(i, i + 3);
+
+    if (codon !== "UAA" && codon !== "UAG" && codon !== "UGA") {
+      translate.push(protein[codon]);
+    } else {
+      break;
+    }
+
+    if (protein[codon] == null) {
+      throw "Invalid codon";
+    }
+  }
+
+  return translate;
 };
