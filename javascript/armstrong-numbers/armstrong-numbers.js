eabfc5626e751c52068f4c595dfa5bc1693c10c4
Elvy_kiung <kiungh22@gmail.com>
2019-12-05 17:23:38 -0800

    alter solution

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -20,3 +20,11 @@ export const validate = input => {
     return false;
   }
 };
+
+// const reducer = (acc, cur, _idx, arr) => acc += Math.pow(cur, arr.length);
+
+// export const validate = (n) => {
+//   const arr = [...n.toString()];
+//   const armstrong = arr.reduce(reducer, 0);
+//   return n === armstrong;
+// };
